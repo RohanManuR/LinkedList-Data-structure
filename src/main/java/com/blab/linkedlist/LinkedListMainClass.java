d91ab8b1c0dc431485dad923a47c2067ce05aef1
rohan <rohanrmanu99@gmail.com>
2025-05-12 13:08:17 +0530

    [ROHAN]CRATE:Created method to perform simple linked list

--- src/main/java/com/blab/linkedlist/LinkedListMainClass.java
+++ src/main/java/com/blab/linkedlist/LinkedListMainClass.java
@@ -1,8 +1,18 @@
 package com.blab.linkedlist;
 
+import java.util.LinkedList;
+
 public class LinkedListMainClass {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        createSimpleLinkedList();
+    }
+
+    public static void createSimpleLinkedList() {
+        LinkedList<Integer> list = new LinkedList();
+        list.add(56);
+        list.add(30);
+        list.add(70);
+        System.out.println(list);
     }
 
 }
